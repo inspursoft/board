b60df09a69d4f294f0f2127173ebc80233a22438
liyanq528 <liyanq528@163.com>
2017-06-30 13:20:51 +0800

    update dashboard template

--- src/ui/src/app/dashboard/dashboard.module.ts
+++ src/ui/src/app/dashboard/dashboard.module.ts
@@ -3,9 +3,15 @@ import { NgModule } from '@angular/core';
 import { SharedModule } from '../shared/shared.module';
 
 import { DashboardComponent } from './dashboard.component';
+import {AngularEchartsModule} from "angular2-echarts";
+import {CommonModule} from "@angular/common";
+import {CoreModule} from "../core/core.module";
 
 @NgModule({
   imports: [
+    CommonModule,
+    CoreModule,
+    AngularEchartsModule,
     SharedModule
   ],
   declarations:[ 
