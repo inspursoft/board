aef65cfa1048fb1a1f8fcd5d4f9ef9c91b214cbc
liyanq528 <liyanq528@163.com>
2017-06-30 18:10:24 +0800

    modify dashboard for add show data

--- src/ui/src/app/dashboard/dashboard.component.ts
+++ src/ui/src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,4 @@
-import {AfterViewInit, Component} from '@angular/core';
+import {AfterViewInit, OnInit, Component} from '@angular/core';
 import {DatePipe} from "@angular/common";
 
 @Component({
@@ -6,15 +6,32 @@ import {DatePipe} from "@angular/common";
     templateUrl: 'dashboard.component.html',
     styleUrls: ['dashboard.component.css']
 })
-export class DashboardComponent implements AfterViewInit {
-    serviceOptions={};
+export class DashboardComponent implements OnInit, AfterViewInit {
+    serviceOptions = {};
+    serviceOptionsOther ={};
+    curData: any = [];
+    curDataOther: any = [];
+    baseDate: Date = new Date();
+    oneStepTime: number = 10 * 1000;
+
+    ngOnInit() {
+        for (let i = 0; i < 11; i++) {
+            let arrBuf = [this.getDate(), this.getRandomData()];
+            this.curData.push(arrBuf);
+            let arrOther = Array.from(arrBuf);
+            arrOther[1] = this.getRandomDataOther();
+            this.curDataOther.push(arrOther)
+        }
+    }
+
     ngAfterViewInit() {
-        this.serviceOptions = {
+        this.serviceOptionsOther = {
             tooltip: {
                 trigger: 'axis',
                 formatter: (params) => {
                     let xDate: Date = new Date(params[0].value[0]);
                     let pDate: DatePipe = new DatePipe("lt");
+                    console.log(params);
                     return pDate.transform(xDate, "yyyy/MM/dd HH:mm:ss") +
                         `<div style='display: flex;flex-direction: column'>
                         <div style="display: flex;align-items: center">
@@ -23,7 +40,7 @@ export class DashboardComponent implements AfterViewInit {
                         </div>
                         <div style="display: flex;align-items: center">
                             <div style='width: 16px;height: 16px; background-color: blue;border-radius: 50%'></div>
-                            <div>containers:${params[0].value[2]}</div>
+                            <div>containers:${params[1].value[1]}</div>
                         </div>
                     </div>`;
                 },
@@ -53,15 +70,7 @@ export class DashboardComponent implements AfterViewInit {
                     smooth: true,
                     symbolSize: 10,
                     hoverAnimation: false,
-                    data: [
-                        [new Date(2017, 6, 29, 0, 0, 0), 4, 6],
-                        [new Date(2017, 6, 29, 2, 0, 0), 10, 9],
-                        [new Date(2017, 6, 29, 4, 0, 0), 20, 2],
-                        [new Date(2017, 6, 29, 5, 0, 0), 40, 10],
-                        [new Date(2017, 6, 29, 6, 0, 0), 50, 60],
-                        [new Date(2017, 6, 29, 8, 0, 0), 30, 50],
-                        [new Date(2017, 6, 29, 10, 0, 0), 70, 35],
-                        [new Date(2017, 6, 29, 12, 0, 0), 65, 40]]
+                    data: this.curData
                 },
                 {
                     // id: '模拟数据',
@@ -70,20 +79,117 @@ export class DashboardComponent implements AfterViewInit {
                     smooth: true,
                     symbolSize: 10,
                     hoverAnimation: false,
-                    data: [
-                        [new Date(2017, 6, 29, 0, 0, 0), 6],
-                        [new Date(2017, 6, 29, 2, 0, 0), 9],
-                        [new Date(2017, 6, 29, 4, 0, 0), 2],
-                        [new Date(2017, 6, 29, 5, 0, 0), 10],
-                        [new Date(2017, 6, 29, 6, 0, 0), 60],
-                        [new Date(2017, 6, 29, 8, 0, 0), 50],
-                        [new Date(2017, 6, 29, 10, 0, 0), 35],
-                        [new Date(2017, 6, 29, 12, 0, 0), 40]]
+                    data: this.curDataOther
                 }
             ]
         };
+        this.serviceOptions = {
+            tooltip: {
+                trigger: 'axis',
+                formatter: (params) => {
+                    let xDate: Date = new Date(params[0].value[0]);
+                    let pDate: DatePipe = new DatePipe("lt");
+                    console.log(params);
+                    return pDate.transform(xDate, "yyyy/MM/dd HH:mm:ss") +
+                        `<div style='display: flex;flex-direction: column'>
+                        <div style="display: flex;align-items: center">
+                            <div style='width: 16px;height: 16px; background-color: red;border-radius: 50%'></div>
+                            <div>pods:${params[0].value[1]}</div>
+                        </div>
+                        <div style="display: flex;align-items: center">
+                            <div style='width: 16px;height: 16px; background-color: blue;border-radius: 50%'></div>
+                            <div>containers:${params[1].value[1]}</div>
+                        </div>
+                    </div>`;
+                },
+                axisPointer: {
+                    animation: false
+                }
+            },
+            xAxis: {
+                type: 'time',
+                splitNumber: 10,
+                splitLine: {
+                    show: false
+                }
+            },
+            yAxis: {
+                type: 'value',
+                show: true,
+                splitLine: {
+                    show: true
+                }
+            },
+            series: [
+                {
+                    // id: '模拟数据',
+                    type: 'line',
+                    showSymbol: true,
+                    smooth: true,
+                    symbolSize: 10,
+                    hoverAnimation: false,
+                    data: this.curData
+                },
+                {
+                    // id: '模拟数据',
+                    type: 'line',
+                    showSymbol: true,
+                    smooth: true,
+                    symbolSize: 10,
+                    hoverAnimation: false,
+                    data: this.curDataOther
+                }
+            ]
+        };
+        setInterval(() => {
+            this.curData.shift();
+            let arrBuf = [this.getDate(), this.getRandomData()];
+            this.curData.push(arrBuf);
+            this.curDataOther.shift();
+            let arrOther = Array.from(arrBuf);
+            arrOther[1] = this.getRandomDataOther();
+            this.curDataOther.push(arrOther);
 
+            this.serviceOptions = {
+                series: [
+                    {
+                        // id: '模拟数据',
+                        type: 'line',
+                        showSymbol: true,
+                        smooth: true,
+                        symbolSize: 10,
+                        hoverAnimation: false,
+                        data: this.curData
+                    },
+                    {
+                        // id: '模拟数据',
+                        type: 'line',
+                        showSymbol: true,
+                        smooth: true,
+                        symbolSize: 10,
+                        hoverAnimation: false,
+                        data: this.curDataOther
+                    }
+                ]
+            };
+        }, 2000);
     }
+
+    getRandomData(): number {//pod
+        return 100 + Math.round(Math.random() * 50);
+    }
+
+    getRandomDataOther(): number {
+        return 300 + Math.round(Math.random() * 100);
+    }
+
+    getDate(): Date {
+        let bDate = this.curData.length > 0 ?
+            this.curData[this.curData.length - 1][0] :
+            this.baseDate;
+        return new Date(bDate.getTime() + this.oneStepTime);
+    }
+
 
     get serviceIcon(): string {
         return '../../images/service_icon.png';
