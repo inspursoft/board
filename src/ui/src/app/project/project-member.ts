79d00340098774219ff4684c6478516cb692e27a
kunw <kunw@vmware.com>
2017-07-07 18:41:55 +0800

    Add project and member to UI.

---
+++ src/ui/src/app/project/project-member.ts
@@ -0,0 +1,6 @@
+import { Member } from './member/member';
+
+export class ProjectMember {
+  projectId: number;
+  members: Member[];
+}
--- src/ui/src/app/project/project.component.ts
+++ src/ui/src/app/project/project.component.ts
@@ -1,7 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { Project } from './project';
+import { ProjectService } from './project.service';
+import { CreateProjectComponent } from './create-project/create-project.component';
+import { MemberComponent } from './member/member.component';
 
 @Component({
   selector: 'project',
   templateUrl: 'project.component.html'
 })
-export class ProjectComponent {}
+export class ProjectComponent implements OnInit {
+  
+  projects: Project[];
+
+  @ViewChild(CreateProjectComponent) createProjectModal;
+  @ViewChild(MemberComponent) memberModal;
+
+  constructor(private projectService: ProjectService){}
+
+  ngOnInit(): void {
+    this.projectService
+      .getProjects()
+      .then(projects=>this.projects = projects);
+  }
+
+  createProject(): void {
+    this.createProjectModal.openModal();
+  }
+
+  editProjectMember(): void {
+    this.memberModal.openModal();
+  }
+}
