82e8e5e6bfc448dc27dd909742d6060ed8621b1d
wangkun_lc <wangkun_lc@inspur.com>
2017-08-02 16:31:24 +0800

    Remove check path in entrypoint script.

--- make/dev/container/uibuilder/entrypoint.sh
+++ make/dev/container/uibuilder/entrypoint.sh
@@ -4,11 +4,6 @@ set -e
 cd /board_src
 rm -rf dist/*
 
-#Check if node_modules directory existing
-if [ ! -d "./node_modules" ]; then
-  mv /board_resource/node_modules ./
-fi
-
 cat ./package.json
 
 npm install
