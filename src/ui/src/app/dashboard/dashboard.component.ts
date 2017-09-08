42ebe07194d4c75778f0db842ac53b7e2c646b7e
liyanq528 <liyanq528@163.com>
2017-09-08 15:13:07 +0800

    odify dashboard

--- src/ui/src/app/dashboard/dashboard.component.ts
+++ src/ui/src/app/dashboard/dashboard.component.ts
@@ -9,9 +9,6 @@ import { Subscription } from "rxjs/Subscription";
 import { Subject } from "rxjs/Subject";
 import { MessageService } from "../shared/message-service/message.service";
 import { promise } from "selenium-webdriver";
-import checkedNodeCall = promise.checkedNodeCall;
-import { max } from "rxjs/operator/max";
-import { min } from "rxjs/operator/min";
 
 const MAX_COUNT_PER_PAGE: number = 200;
 const MAX_COUNT_PER_DRAG: number = 100;
