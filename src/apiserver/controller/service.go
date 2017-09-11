fbed28a0a4482ad6cbddd6ba1644202442f27863
Tim Luo(罗天) <luotian@inspur.com>
2017-09-11 11:06:01 +0800

    merge to new git log

--- src/apiserver/controller/service.go
+++ src/apiserver/controller/service.go
@@ -5,18 +5,34 @@ import (
 	"fmt"
 	"git/inspursoft/board/src/common/model"
 	//"io/ioutil"
+	"git/inspursoft/board/src/apiserver/service"
+	"net/http"
+	"path/filepath"
+	"strconv"
+
+	"github.com/astaxie/beego/logs"
 )
 
 type ServiceController struct {
 	baseController
 }
 
+//  Checking the user priviledge by token
+func (p *ServiceController) Prepare() {
+	user := p.getCurrentUser()
+	if user == nil {
+		p.CustomAbort(http.StatusUnauthorized, "Need to login first.")
+		return
+	}
+	p.currentUser = user
+	p.isSysAdmin = (user.SystemAdmin == 1)
+	p.isProjectAdmin = (user.ProjectAdmin == 1)
+}
+
 // API to deploy service
 func (p *ServiceController) DeployServiceAction() {
 	var err error
-	//var basepath = repoPath
-	//var projectname = ""
-	//var content = []byte("deployment yaml\n")
+
 	reqData, err := p.resolveBody()
 	if err != nil {
 		p.internalError(err)
@@ -30,8 +46,47 @@ func (p *ServiceController) DeployServiceAction() {
 		return
 	}
 	fmt.Println(reqServiceConfig)
-	//filename := basepath + projectname + "deploymentyaml"
-	//ioutil.WriteFile(filename, content, 0666)
-	//TODO implement service deployment api
-	p.serveStatus(200, "deploy service successfully")
+
+	//TODO check valid()
+
+	var serviceConfigPath = filepath.Join(repoPath,
+		reqServiceConfig.ProjectName, strconv.FormatInt(reqServiceConfig.ServiceID, 10))
+
+	logs.Info("Service config path: %s", serviceConfigPath)
+	service.SetDeploymentPath(serviceConfigPath)
+
+	//Build deployment yaml file
+	err = service.BuildDeploymentYml(reqServiceConfig)
+	if err != nil {
+		logs.Info("Build Deployment Yaml failed")
+		p.internalError(err)
+		return
+	}
+
+	//Build deployment yaml file
+	err = service.BuildServiceYml(reqServiceConfig)
+	if err != nil {
+		logs.Info("Build Service Yaml failed")
+		p.internalError(err)
+		return
+	}
+
+	//TODO: push service deployment to jenkins
+	//push to git
+	/*
+			var pushobject pushObject
+
+		    pushobject.FileName = "service.yaml and deployment.yaml"
+		    pushobject.JobName = "process_service"
+		    pushobject.Message = "upload deployment file"
+		    pushobject.Value = loadPath
+
+			ret, msg, err := InternalPushObjects(&pushobject, &(p.baseController))
+			if err != nil {
+				p.internalError(err)
+				return
+			}
+			logs.Info("Internal push object: %d %s", ret, msg)
+			p.CustomAbort(ret, msg)
+	*/
 }
