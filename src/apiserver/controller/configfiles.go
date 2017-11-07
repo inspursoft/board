32d50221f8fd4b8ee1754d727fad20bba1ffaf08
wei_ting <wei_ting@inspur.com>
2017-11-07 08:55:03 +0800

    export/import dockerfile

--- src/apiserver/controller/configfiles.go
+++ src/apiserver/controller/configfiles.go
@@ -37,12 +37,12 @@ func (f *ConfigFilesController) Prepare() {
 
 func (f *ConfigFilesController) UploadDeploymentYamlFileAction() {
 	projectName := f.GetString("project_name")
-	isExist, err := isProjectName(projectName)
+	isExistence, err := service.ProjectExists(projectName)
 	if err != nil {
 		f.internalError(err)
 		return
 	}
-	if isExist != true {
+	if isExistence != true {
 		f.customAbort(http.StatusBadRequest, "Project don't exist.")
 		return
 	}
@@ -54,7 +54,7 @@ func (f *ConfigFilesController) UploadDeploymentYamlFileAction() {
 		return
 	}
 	if serviceID == "" {
-		serviceID, err = CreateServiceConfig(projectName, f.currentUser.ID)
+		serviceID, err = createServiceConfig(projectName, f.currentUser.ID)
 		if err != nil {
 			f.internalError(err)
 			return
@@ -63,13 +63,13 @@ func (f *ConfigFilesController) UploadDeploymentYamlFileAction() {
 	}
 
 	yamlType := f.GetString("yaml_type")
-	fileName, flag := GetYamlFileName(yamlType)
-	if flag == false {
+	fileName := getYamlFileName(yamlType)
+	if fileName == "" {
 		f.customAbort(http.StatusBadRequest, "yaml type invalid.")
 		return
 	}
 	targetFilePath := filepath.Join(repoPath, projectName, serviceID)
-	logs.Info("User: %s uploaded deployment yaml file to %s.", f.currentUser.Username, targetFilePath)
+	logs.Info("User: %s uploaded %s yaml file to %s.", f.currentUser.Username, yamlType, targetFilePath)
 
 	err = f.SaveToFile("upload_file", filepath.Join(targetFilePath, fileName))
 	if err != nil {
@@ -80,12 +80,12 @@ func (f *ConfigFilesController) UploadDeploymentYamlFileAction() {
 
 func (f *ConfigFilesController) DownloadDeploymentYamlFileAction() {
 	projectName := f.GetString("project_name")
-	isExist, err := isProjectName(projectName)
+	isExistence, err := service.ProjectExists(projectName)
 	if err != nil {
 		f.internalError(err)
 		return
 	}
-	if isExist != true {
+	if isExistence != true {
 		f.customAbort(http.StatusBadRequest, "Project name invalid.")
 		return
 	}
@@ -103,15 +103,70 @@ func (f *ConfigFilesController) DownloadDeploymentYamlFileAction() {
 
 	//get paras
 	yamlType := f.GetString("yaml_type")
-	fileName, flag := GetYamlFileName(yamlType)
-	if flag == false {
+	fileName := getYamlFileName(yamlType)
+	if fileName == "" {
 		f.customAbort(http.StatusBadRequest, "yaml type invalid.")
 		return
 	}
 	absFileName := filepath.Join(repoPath, projectName, serviceID, fileName)
-	logs.Info("User: %s download deployment yaml file from %s.", f.currentUser.Username, absFileName)
+	logs.Info("User: %s download %s yaml file from %s.", f.currentUser.Username, yamlType, absFileName)
 
 	f.Ctx.Output.Download(absFileName, fileName)
+}
+
+func (f *ConfigFilesController) UploadDockerfileFileAction() {
+	projectName := f.GetString("project_name")
+	isExistence, err := service.ProjectExists(projectName)
+	if err != nil {
+		f.internalError(err)
+		return
+	}
+	if isExistence != true {
+		f.customAbort(http.StatusBadRequest, "Project don't exist.")
+		return
+	}
+
+	imageName := f.GetString("image_name")
+	tagName := f.GetString("tag_name")
+	targetFilePath := filepath.Join(repoPath, projectName, imageName, tagName)
+	err = os.MkdirAll(targetFilePath, 0755)
+	if err != nil {
+		f.internalError(err)
+		return
+	}
+	logs.Info("User: %s uploaded Dockerfile file to %s.", f.currentUser.Username, targetFilePath)
+
+	err = f.SaveToFile("upload_file", filepath.Join(targetFilePath, "Dockerfile"))
+	if err != nil {
+		f.internalError(err)
+	}
+
+}
+
+func (f *ConfigFilesController) DownloadDockerfileFileAction() {
+	projectName := f.GetString("project_name")
+	isExistence, err := service.ProjectExists(projectName)
+	if err != nil {
+		f.internalError(err)
+		return
+	}
+	if isExistence != true {
+		f.customAbort(http.StatusBadRequest, "Project name invalid.")
+		return
+	}
+
+	imageName := f.GetString("image_name")
+	tagName := f.GetString("tag_name")
+	targetFilePath := filepath.Join(repoPath, projectName, imageName, tagName)
+	if _, err := os.Stat(targetFilePath); os.IsNotExist(err) {
+		f.customAbort(http.StatusBadRequest, "image Name and  tag name are invalid.")
+		return
+	}
+
+	absFileName := filepath.Join(repoPath, projectName, imageName, tagName, "Dockerfile")
+	logs.Info("User: %s download Dockerfile file from %s.", f.currentUser.Username, absFileName)
+
+	f.Ctx.Output.Download(absFileName, "Dockerfile")
 }
 
 func getServiceID(serviceName string, projectName string) (string, error) {
@@ -123,19 +178,21 @@ func getServiceID(serviceName string, projectName string) (string, error) {
 		return "", err
 	}
 	if service == nil {
-		logs.Info("service is not exist")
+		logs.Info("service doesn't exist")
 		return "", nil
 	}
 	return strconv.Itoa(int(service.ID)), nil
 }
 
-func CreateServiceConfig(projectName string, ID int64) (string, error) {
+func createServiceConfig(projectName string, ID int64) (string, error) {
 	//get ProjectID
-	ProjectID, err := GetProjectID(projectName)
+	query := model.Project{Name: projectName}
+	project, err := service.GetProject(query, "name")
 	if err != nil {
 		return "", err
 	}
-	if ProjectID == 0 {
+	if project == nil {
+		logs.Info("project doesn't exist")
 		return "", errors.New("project is invalid")
 	}
 
@@ -143,7 +200,7 @@ func CreateServiceConfig(projectName string, ID int64) (string, error) {
 	newservice.ProjectName = projectName
 	newservice.Status = preparing // 0: preparing 1: running 2: suspending
 	newservice.OwnerID = ID
-	newservice.ProjectID = ProjectID
+	newservice.ProjectID = project.ID
 	serviceID, err := service.CreateServiceConfig(newservice)
 	if err != nil {
 		return "", err
@@ -151,50 +208,14 @@ func CreateServiceConfig(projectName string, ID int64) (string, error) {
 	return strconv.Itoa(int(serviceID)), err
 }
 
-func GetProjectID(projectName string) (int64, error) {
-	query := model.Project{Name: projectName}
-	project, err := service.GetProject(query, "name")
-	if err != nil {
-		return 0, err
-	}
-	if project == nil {
-		logs.Info("project is not exist")
-		return 0, nil
-	}
-	return project.ID, nil
-}
-
-func isProjectName(projectName string) (bool, error) {
-	projectExists, err := service.ProjectExists(projectName)
-	if err != nil {
-		return false, err
-	}
-	return projectExists, nil
-}
-
-func CreateProject(projectName string, ID int, userName string) error {
-	var reqProject model.Project
-	reqProject.Name = projectName
-	reqProject.OwnerID = ID
-	reqProject.OwnerName = userName
-	isSuccess, err := service.CreateProject(reqProject)
-	if err != nil {
-		return err
-	}
-	if !isSuccess {
-		return errors.New("Project name is illegal.")
-	}
-	return nil
-}
-
-func GetYamlFileName(yamlType string) (string, bool) {
+func getYamlFileName(yamlType string) string {
 	var fileName string
 	if yamlType == deploymentType {
 		fileName = deploymentFilename
 	} else if yamlType == serviceType {
 		fileName = serviceFilename
 	} else {
-		return "", false
+		return ""
 	}
-	return fileName, true
+	return fileName
 }
