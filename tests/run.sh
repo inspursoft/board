0b0af21d108feb49a712e03e51699e92c5de2838
root <yingyangu@163.com>
2017-11-28 15:01:47 +0800

    go used docker container

--- tests/run.sh
+++ tests/run.sh
@@ -1,6 +1,7 @@
 # listDeps lists packages referenced by package in $1, 
 # excluding golang standard library and packages in 
 # direcotry vendor
+
 function listDeps()
 {
     pkg=$1
@@ -19,9 +20,15 @@ function listDeps()
 set -e
 echo "mode: set" >profile.cov
 
+# set envirnment
 deps=""
-packages=$(go list ../... | grep -v -E 'vendor|tests')
+gop=/go/src/git/inspursoft/board/
+goversion=golang:1.8.3-alpine3.5
+dock=/usr/bin/docker
+var=`pwd`
+PWD=`dirname $var`
 
+packages=$(go list ../... | grep -v -E 'vendor|tests')
 for package in $packages
 do
     listDeps $package
@@ -33,13 +40,17 @@ do
     echo $deps
     echo $package
     echo "+++++++++++++++++++++++++++++++++++++++"
-    go test -v -race -cover -coverprofile=profile.tmp -coverpkg "$deps" $package
-    if [ -f profile.tmp ]
+    
+    #go env used docker container
+    echo "$dock run --rm -v $PWD:$gop -w $gop $goversion go test -v -cover -coverprofile=profile.tmp -coverpkg "$deps" $package"
+    /usr/bin/docker run --rm -v $PWD:$gop -w $gop $goversion go test -v -cover -coverprofile=profile.tmp -coverpkg "$deps" $package
+
+    if [ -f $PWD/profile.tmp ]
     then
-        cat profile.tmp | tail -n +2 >> profile.cov
-        rm profile.tmp
+        cat $PWD/profile.tmp | tail -n +2 >> profile.cov
+        rm $PWD/profile.tmp
      fi
+done
 go tool cover -func=profile.cov > out.temp
 go tool cover -html=profile.cov -o profile.html
-done
 
