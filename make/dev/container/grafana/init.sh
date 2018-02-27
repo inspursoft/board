8cf3ad3764fa6eacb32148cb56a5229a972414cd
zhanghu01 <zhanghu01@inspur.com>
2018-02-27 15:57:33 +0800

    hide some grafana configuration about graphite. adjust the directory struct.

--- make/dev/container/grafana/init.sh
+++ make/dev/container/grafana/init.sh
@@ -32,27 +32,27 @@ function init()
         sleeptime=5
         # check server status
         echo "checking grafana server"
-        util_done $sleeptime curl ${GRAFANA_ADDRESS}/api/org
+        util_done $sleeptime curl http://grafana:3000/api/org
         echo "grafana server has already started"
 
         # set the kubernetes plugin
         echo "setting the kubernetes plugin"
-        util_done $sleeptime curl -X POST -H "Content-Type: application/json;charset=UTF-8" -d @/etc/grafana/config/kubernetes.json ${GRAFANA_ADDRESS}/api/plugins/raintank-kubernetes-app/settings
+        util_done $sleeptime curl -X POST -H "Content-Type: application/json;charset=UTF-8" -d @/etc/grafana/config/kubernetes.json http://grafana:3000/api/plugins/raintank-kubernetes-app/settings
         echo "set the kubernetes plugin successfully"
 
         # add the graphite datasource
         echo "adding the graphite datasource to grafana"
-        util_done $sleeptime curl -X POST -H "Content-Type: application/json;charset=UTF-8" -d @/etc/grafana/config/graphite-datasource.json ${GRAFANA_ADDRESS}/api/datasources
+        util_done $sleeptime curl -X POST -H "Content-Type: application/json;charset=UTF-8" -d @/etc/grafana/config/graphite-datasource.json http://grafana:3000/api/datasources
         echo "add the graphite datasource to grafana successfully"
 
         # add the kubernetes datasource
         echo "adding the kubernetes datasource to grafana"
-        util_done $sleeptime curl -X POST -H "Content-Type: application/json;charset=UTF-8" -d @/etc/grafana/config/kubernetes-datasource.json ${GRAFANA_ADDRESS}/api/datasources
+        util_done $sleeptime curl -X POST -H "Content-Type: application/json;charset=UTF-8" -d @/etc/grafana/config/kubernetes-datasource.json http://grafana:3000/api/datasources
         echo "add the kubernetes datasource to grafana successfully"
 
         # add the kuberenets dashboard
         echo "adding the kubernetes dashboard to grafana"
-        util_done $sleeptime curl -X POST -H "Content-Type: application/json;charset=UTF-8" -d @/etc/grafana/config/kubernetes-dashboard.json ${GRAFANA_ADDRESS}/api/dashboards/db
+        util_done $sleeptime curl -X POST -H "Content-Type: application/json;charset=UTF-8" -d @/etc/grafana/config/kubernetes-dashboard.json http://grafana:3000/api/dashboards/db
         echo "add the kubernetes dashboard to grafana successfully"
 
         # generate the install tag file 
