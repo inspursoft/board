75e27b3dc618daa7b5fe530a7e66ab96c48aaaa8
zhanghu01 <zhanghu01@inspur.com>
2018-02-27 13:27:23 +0800

    move grafana configuration file to template directory, then run the container with the mounted directory /etc/grafana/config.

--- make/dev/container/grafana/init.sh
+++ make/dev/container/grafana/init.sh
@@ -14,19 +14,20 @@ function util_done()
     done
 }
 
-function subst()
-{
-    perl -p -e 's/\$\{([^}]+)\}/defined $ENV{$1} ? $ENV{$1} : $&/eg; s/\$\{([^}]+)\}//eg' $1
-}
-
 function init()
 {
     if [ ! -f /var/lib/grafana/initok ]; then
         #replace environment variables
-        subst /etc/grafana/config/kubernetes.json 1>/tmp/kubernetes.generated
-        subst /etc/grafana/config/graphite-datasource.json 1>/tmp/graphite-datasource.generated
-        subst /etc/grafana/config/kubernetes-datasource.json 1>/tmp/kubernetes-datasource.generated
-        subst /etc/grafana/config/kubernetes-dashboard.json 1>/tmp/kubernetes-dashboard.generated
+        if [ ! -d /etc/grafana/config/ ]; then
+            echo "directory /etc/grafana/config/ does not exist. make sure you have executed the 'make prepare' command"
+            exit 1
+        fi
+
+        allfiles=`ls -l /etc/grafana/config/ | wc -l`
+        if [ $allfiles == "1" ]; then
+            echo "directory /etc/grafana/config/ does not have files. make sure you have executed the 'make prepare' command"
+            exit 2
+        fi
 
         sleeptime=5
         # check server status
@@ -36,22 +37,22 @@ function init()
 
         # set the kubernetes plugin
         echo "setting the kubernetes plugin"
-        util_done $sleeptime curl -X POST -H "Content-Type: application/json;charset=UTF-8" -d @/tmp/kubernetes.generated ${GRAFANA_ADDRESS}/api/plugins/raintank-kubernetes-app/settings
+        util_done $sleeptime curl -X POST -H "Content-Type: application/json;charset=UTF-8" -d @/etc/grafana/config/kubernetes.json ${GRAFANA_ADDRESS}/api/plugins/raintank-kubernetes-app/settings
         echo "set the kubernetes plugin successfully"
 
         # add the graphite datasource
         echo "adding the graphite datasource to grafana"
-        util_done $sleeptime curl -X POST -H "Content-Type: application/json;charset=UTF-8" -d @/tmp/graphite-datasource.generated ${GRAFANA_ADDRESS}/api/datasources
+        util_done $sleeptime curl -X POST -H "Content-Type: application/json;charset=UTF-8" -d @/etc/grafana/config/graphite-datasource.json ${GRAFANA_ADDRESS}/api/datasources
         echo "add the graphite datasource to grafana successfully"
 
         # add the kubernetes datasource
         echo "adding the kubernetes datasource to grafana"
-        util_done $sleeptime curl -X POST -H "Content-Type: application/json;charset=UTF-8" -d @/tmp/kubernetes-datasource.generated ${GRAFANA_ADDRESS}/api/datasources
+        util_done $sleeptime curl -X POST -H "Content-Type: application/json;charset=UTF-8" -d @/etc/grafana/config/kubernetes-datasource.json ${GRAFANA_ADDRESS}/api/datasources
         echo "add the kubernetes datasource to grafana successfully"
 
         # add the kuberenets dashboard
         echo "adding the kubernetes dashboard to grafana"
-        util_done $sleeptime curl -X POST -H "Content-Type: application/json;charset=UTF-8" -d @/tmp/kubernetes-dashboard.generated ${GRAFANA_ADDRESS}/api/dashboards/db
+        util_done $sleeptime curl -X POST -H "Content-Type: application/json;charset=UTF-8" -d @/etc/grafana/config/kubernetes-dashboard.json ${GRAFANA_ADDRESS}/api/dashboards/db
         echo "add the kubernetes dashboard to grafana successfully"
 
         # generate the install tag file 
@@ -59,12 +60,5 @@ function init()
         echo "init successfully" > /var/lib/grafana/initok
     fi
 }
-
-export GRAFANA_ADDRESS=${GRAFANA_ADDRESS-http://127.0.0.1:3000}
-export GRAPHITE_PROTOCOL=${GRAPHITE_PROTOCOL-http}
-export GRAPHITE_IP=${GRAPHITE_IP-graphite}
-export GRAPHITE_PORT=${GRAPHITE_PORT-80}
-export GRAPHITE_CARBON_PORT=${GRAPHITE_CARBON_PORT-2003}
-export KUBERNETES_ADDRESS=${KUBERNESTES_ADDRESS-http://${KUBE_MASTER_IP}:${KUBE_MASTER_PORT}}
 
 init &
