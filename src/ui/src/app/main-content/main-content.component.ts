6a47872a0a9235acc19b38516b14155e2f26d37b
liyanq528 <liyanq528@163.com>
2018-02-28 10:34:22 +0800

    modify grafana url

--- src/ui/src/app/main-content/main-content.component.ts
+++ src/ui/src/app/main-content/main-content.component.ts
@@ -33,7 +33,7 @@ export class MainContentComponent {
     });
     let systemInfo = this.route.snapshot.data['systeminfo'];
     this.appInitService.systemInfo = systemInfo;
-    this.appInitService.grafanaViewUrl = `http://${systemInfo['board_host']}:3000/dashboard/db/kubernetes-cluster?refresh=30s&orgId=1`;
+    this.appInitService.grafanaViewUrl = `http://${systemInfo['board_host']}:3000/dashboard/db/kubernetes/`;
   }
 
   get grafanaViewUrl():string{
