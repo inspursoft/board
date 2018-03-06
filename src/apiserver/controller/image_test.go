b730f5e5c66e3227b8d4b4ed05657f6ecde2c275
maoze <maoze@inspur.com>
2018-03-06 13:41:12 +0800

    add image and auth test code

--- src/apiserver/controller/image_test.go
+++ src/apiserver/controller/image_test.go
@@ -30,3 +30,18 @@ func TestGetImageRegistryAction(t *testing.T) {
 	assert.Equal(http.StatusOK, w.Code, "Get registry fail.")
 	logs.Info("Tested GetImageRegistry %s pass", w.Body.String())
 }
+
+func TestGetImagesAction(t *testing.T) {
+	assert := assert.New(t)
+	token := signIn("admin", "123456a?")
+	defer signOut("admin")
+	assert.NotEmpty(token, "signIn error")
+
+	reqURL := "/api/v1/images?token=" + token
+	r, _ := http.NewRequest("GET", reqURL, nil)
+	w := httptest.NewRecorder()
+	beego.BeeApp.Handlers.ServeHTTP(w, r)
+
+	assert.Equal(http.StatusOK, w.Code, "Get images fail.")
+	logs.Info("Tested GetImagesAction %s pass", w.Body.String())
+}
