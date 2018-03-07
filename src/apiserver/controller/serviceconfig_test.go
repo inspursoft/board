2fd51d5291c3ae42731a0261a3bb3c360d7340e7
wei_ting <wei_ting@inspur.com>
2018-03-07 10:53:21 +0800

    refine service config test case

--- src/apiserver/controller/serviceconfig_test.go
+++ src/apiserver/controller/serviceconfig_test.go
@@ -3,7 +3,6 @@ package controller
 import (
 	"bytes"
 	"encoding/json"
-	"errors"
 	"git/inspursoft/board/src/common/model"
 	"net/http"
 	"net/http/httptest"
@@ -39,16 +38,17 @@ var configServiceStep = model.ConfigServiceStep{
 	},
 }
 
-func setConfigServiceStepAction() error {
+func TestSetConfigServiceStepAction(t *testing.T) {
+	assert := assert.New(t)
 	token := signIn("admin", "123456a?")
-	if token == "" {
-		return errors.New("ERR_SIGNIN")
-	}
+	assert.NotEmpty(token, "Error occurred while sign in Board")
+	defer func() {
+		err := signOut("admin")
+		assert.Nil(err, "Error occurred while sign out Board")
+	}()
 
 	body, err := json.Marshal(configServiceStep)
-	if err != nil {
-		return errors.New("ERR_MARSHAL_SERVICE_CONFIG")
-	}
+	assert.Nil(err, "Error occurred while testing SetConfigServiceStepAction.")
 
 	//config service
 	r, _ := http.NewRequest("POST", "/api/v1/services/config", bytes.NewReader(body))
@@ -58,18 +58,5 @@ func setConfigServiceStepAction() error {
 	r.URL.RawQuery = phase.Encode()
 	w := httptest.NewRecorder()
 	beego.BeeApp.Handlers.ServeHTTP(w, r)
-	if w.Code != http.StatusOK {
-		return errors.New("ERR_SET_SERVICE_CONFIG_ACTION")
-	}
-
-	err = signOut("admin")
-	if err != nil {
-		return errors.New("ERR_SIGNOUT")
-	}
-	return nil
-}
-func TestSetConfigServiceStepAction(t *testing.T) {
-	assert := assert.New(t)
-	err := setConfigServiceStepAction()
-	assert.Empty(err, "SetConfigServiceStepAction error.")
+	assert.Equal(w.Code, http.StatusOK, "Error occurred while testing SetConfigServiceStepAction.")
 }
