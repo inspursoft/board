9e47ac7c593e128e901fea75c2a60ff303f1a2c0
root <yingyangu@163.com>
2018-03-07 15:06:21 +0800

    add ui builder test

--- tests/jenkinsrun.sh
+++ tests/jenkinsrun.sh
@@ -44,7 +44,7 @@ docker-compose -f docker-compose.test.yml down -v
 docker-compose -f docker-compose.test.yml up -d
 
 
-#docker-compose -f docker-compose.uibuilder.test.yml up 
+docker-compose -f docker-compose.uibuilder.test.yml up 
 
 export GOPATH=$workDir
 
@@ -57,7 +57,7 @@ chmod +x *
 #make run
 ./run.sh $host_ip $kube_master_url $node_ip $registry_uri
 
-cp -r /home/tests/testresult.log /home/tests/coverage/ $uiDir
+#cp -r /home/tests/testresult.log /home/tests/coverage/ $uiDir
 uiCoverage=`cat $uiDir/testresult.log |grep "Statements"|cut -d ":" -f 2|cut -d "%" -f 1|awk 'gsub(/^ *| *$/,"")'`
 
 cov=`cat $boardDir/$branchDir/tests/out.temp|grep "total"|awk '{print $NF}'|cut -d "%" -f 1|tr -s [:space:]`
