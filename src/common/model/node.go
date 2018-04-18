edbf158d814215bfa47cd7b5aa0aeb7e1e96d82b
Tim Luo(罗天) <luotian@inspur.com>
2018-04-18 11:10:56 +0800

    add node group in dao

--- src/common/model/node.go
+++ src/common/model/node.go
@@ -8,10 +8,10 @@ type NodeGroup struct {
 	ID           int64     `json:"nodegroup_id" orm:"column(id)"`
 	GroupName    string    `json:"nodegroup_name" orm:"column(name)"`
 	Comment      string    `json:"nodegroup_comment" orm:"column(comment)"`
-        OwnerID      int64     `json:"nodegroup_owner_id" orm:"column(owner_id)"`
+	OwnerID      int64     `json:"nodegroup_owner_id" orm:"column(owner_id)"`
 	CreationTime time.Time `json:"nodegroup_creation_time" orm:"column(creation_time)"`
 	UpdateTime   time.Time `json:"nodegroup_update_time" orm:"column(update_time)"`
 	Deleted      int       `json:"nodegroup_deleted" orm:"column(deleted)"`
 	Project      string    `json:"nodegroup_project" orm:"column(project_name)"`
-        ProjectID    int64     `json:"nodegroup_project" orm:"column(project_id)"` 
+	ProjectID    int64     `json:"nodegroup_project" orm:"column(project_id)"`
 }
