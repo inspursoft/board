c04e3a476a2a4f0c0a2e8d1d0257cb30265aa49b
Tim Luo(罗天) <luotian@inspur.com>
2018-05-21 15:09:23 +0800

    add scale in k8sassist

--- src/common/k8sassist/corev1/cgv5/apps/scaleassist.go
+++ src/common/k8sassist/corev1/cgv5/apps/scaleassist.go
@@ -2,23 +2,43 @@
 package apps
 
 import (
+	"git/inspursoft/board/src/common/k8sassist/corev1/cgv5/types"
 	"git/inspursoft/board/src/common/model"
+
+	"github.com/astaxie/beego/logs"
+	"k8s.io/client-go/kubernetes/typed/extensions/v1beta1"
 )
 
 type scales struct {
 	namespace string
+	scale     v1beta1.ScaleInterface
 }
 
-func (d *scales) Update(kind string, scale *model.Scale) (*model.Scale, error) {
-	return nil, nil
+func (s *scales) Update(kind string, scale *model.Scale) (*model.Scale, error) {
+	k8sScale := types.ToK8sScale(scale)
+	newk8sScale, err := s.scale.Update(kind, k8sScale)
+	if err != nil {
+		logs.Error("Update Scale of %s/%s failed. Err:%+v", scale.Name, s.namespace, err)
+		return nil, err
+	}
+
+	modelScale := types.FromK8sScale(newk8sScale)
+	return modelScale, nil
 }
 
-func (d *scales) Get(kind string, name string) (*model.Scale, error) {
-	return nil, nil
+func (s *scales) Get(kind string, name string) (*model.Scale, error) {
+	scaleinstance, err := s.scale.Get(kind, name)
+	if err != nil {
+		logs.Error("Get scale of %s failed. Err:%+v", name, err)
+		return nil, err
+	}
+
+	return types.FromK8sScale(scaleinstance), nil
 }
 
-func NewScales(namespace string) *scales {
+func NewScales(namespace string, scale v1beta1.ScaleInterface) *scales {
 	return &scales{
 		namespace: namespace,
+		scale:     scale,
 	}
 }
