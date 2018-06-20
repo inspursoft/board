e1de10d01ebdacb9f86a8de5073575de2a52d538
wangkun_lc <wangkun_lc@inspur.com>
2018-06-20 12:03:55 +0800

    Update for test waiting.

--- src/common/dao/base_test.go
+++ src/common/dao/base_test.go
@@ -6,6 +6,7 @@ import (
 	"git/inspursoft/board/src/common/utils"
 	"os"
 	"testing"
+	"time"
 
 	"github.com/astaxie/beego/orm"
 )
@@ -15,7 +16,7 @@ func TestMain(m *testing.M) {
 	dbIP := utils.GetStringValue("DB_IP")
 	dbPort := utils.GetStringValue("DB_PORT")
 	dbPassword := utils.GetStringValue("DB_PASSWORD")
-
+	time.Sleep(5)
 	orm.RegisterDriver("mysql", orm.DRMySQL)
 	orm.RegisterDataBase("default", "mysql", fmt.Sprintf("root:%s@tcp(%s:%s)/board?charset=utf8", dbPassword, dbIP, dbPort))
 	model.InitModelDB()
