cf063d99b617cf2cfa57e3b35ee2f0b02490aa9d
liyanqing <liyanq528@163.com>
2018-08-01 17:19:42 +0800

    add auto navigate to sign-in

--- src/ui/src/app/shared/error-pages/bad-gateway.component/bad-gateway.component.ts
+++ src/ui/src/app/shared/error-pages/bad-gateway.component/bad-gateway.component.ts
@@ -1,8 +1,17 @@
-import { Component } from "@angular/core"
+import { Component, OnDestroy } from "@angular/core"
+import { Router } from "@angular/router";
 
 @Component({
   templateUrl: "./bad-gateway.component.html"
 })
-export class BadGatewayComponent {
+export class BadGatewayComponent implements OnDestroy {
+  intervalHandle: any;
 
+  constructor(private route: Router) {
+    this.intervalHandle = setInterval(() => this.route.navigate(["/sign-in"]), 5000);
+  }
+
+  ngOnDestroy() {
+    clearInterval(this.intervalHandle);
+  }
 }
