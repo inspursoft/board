2349afe4a2fc45068d4b326fa435eb7f491cee34
liyanqing <liyanq528@163.com>
2018-09-28 15:35:59 +0800

    fixed issue #1716

--- src/ui/src/app/shared/cs-components-library/cs-dialog/cs-dialog.component.ts
+++ src/ui/src/app/shared/cs-components-library/cs-dialog/cs-dialog.component.ts
@@ -1,35 +1,35 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, HostBinding, HostListener, OnDestroy } from '@angular/core';
 import { BUTTON_STYLE, Message, RETURN_STATUS } from '../../shared.types';
 import { Observable, Subject } from 'rxjs';
-import { Subscription } from "rxjs/Subscription";
 import "rxjs/add/observable/fromEvent"
 
 @Component({
   templateUrl: "./cs-dialog.component.html"
 })
-export class CsDialogComponent implements OnDestroy, OnInit {
+export class CsDialogComponent implements OnDestroy, AfterViewInit {
   opened: boolean;
   curMessage: Message;
-  private keypressSubscribe: Subscription;
   private returnSubject: Subject<Message>;
+  @HostBinding('tabindex') tabIndex = '-1';
 
-  constructor() {
+  @HostListener('keypress', ['$event']) onKeypress(event: KeyboardEvent) {
+    if (event.charCode === 13 && this.curMessage.buttonStyle == BUTTON_STYLE.ONLY_CONFIRM) {
+      this.curMessage.returnStatus = RETURN_STATUS.rsCancel;
+      this.returnSubject.next(this.curMessage);
+    }
+  }
+
+  constructor(private el: ElementRef) {
     this.curMessage = new Message();
     this.returnSubject = new Subject<Message>();
   }
 
-  ngOnDestroy(): void {
-    this.keypressSubscribe.unsubscribe();
+  ngAfterViewInit() {
+    (this.el.nativeElement as HTMLElement).focus();
   }
 
-  ngOnInit() {
-    const obsKeyPress = Observable.fromEvent(document, 'keypress');
-    this.keypressSubscribe = obsKeyPress.subscribe((event: KeyboardEvent) => {
-      if (event.charCode === 13 && this.curMessage.buttonStyle == BUTTON_STYLE.ONLY_CONFIRM) {
-        this.curMessage.returnStatus = RETURN_STATUS.rsCancel;
-        this.returnSubject.next(this.curMessage);
-      }
-    });
+  ngOnDestroy(): void {
+
   }
 
   public openDialog(message:Message):Observable<Message>{
