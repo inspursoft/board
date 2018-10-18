ec0270ae000543c114a4862f58404743ae9e0e58
Your Name <root@localhost.localdomain>
2018-10-18 15:20:39 +0800

    test

--- tests/report.sh
+++ tests/report.sh
@@ -128,7 +128,8 @@ echo "comment_url	:"$comment_url
 echo "info1		:"$info1
 echo "uiinfo		:"$uiinfo
 
-elif [ "$action" == "pull_request" ]; then
+elif [ "$action" == "pull_request" ]; then\
+
 cmd="curl -X POST "$gogs_url/commit-report" -H "Content-Type: application/json" -d "{\"commit_id\": \"${commit_id}\", \"report\": \"build_cov%; ui_cov%|${jenkins_master_url}/job/${base_repo_name}/${build_number}/console\"}""
 echo $cmd
 eval $cmd 
