48684d9357b294929c97add3e4322cde44174ec6
Your Name <root@localhost.localdomain>
2018-10-19 17:48:16 +0800

    test

--- tests/report.sh
+++ tests/report.sh
@@ -138,6 +138,7 @@ echo "uiinfo		:"$uiinfo
 
 elif [ "$action" == "push" ]; then
 cmd="curl -X POST '$gogs_url/commit-report' -H 'Content-Type: application/json' -d '{ \"commit_id\": \"${commit_id}\", \"report\": \"$result|${jenkins_master_url}/job/${base_repo_name}/${build_number}/console\"}'"
+
 echo $cmd
 eval $cmd 
 fi
