f83c628ffa8d821252735b5515960c6323068e43
Liyanqing <liyanq528@163.com>
2019-02-26 14:12:27 +0800

    add resource feature

--- src/ui/src/app/resource/resource.module.ts
+++ src/ui/src/app/resource/resource.module.ts
@@ -2,17 +2,22 @@ import { NgModule } from "@angular/core";
 import { SharedModule } from "../shared/shared.module";
 import { ConfigMapListComponent } from "./config-map/config-map-list/config-map-list.component";
 import { ResourceService } from "./resource.service";
+import { CreateConfigMapComponent } from "./config-map/create-config-map/create-config-map.component";
+import { ConfigMapDetailComponent } from "./config-map/config-map-detail/config-map-detail.component";
 
 @NgModule({
   imports: [SharedModule],
   declarations: [
-    ConfigMapListComponent
+    ConfigMapListComponent,
+    CreateConfigMapComponent,
+    ConfigMapDetailComponent
   ],
-  entryComponents:[
-
+  entryComponents: [
+    CreateConfigMapComponent,
+    ConfigMapDetailComponent
   ],
-  providers:[
-   ResourceService
+  providers: [
+    ResourceService
   ]
 })
 export class ResourceModule {
--- src/ui/src/app/resource/resource.service.ts
+++ src/ui/src/app/resource/resource.service.ts
@@ -1,6 +1,46 @@
 import { Injectable } from "@angular/core";
+import { HttpClient, HttpResponse } from "@angular/common/http";
+import { Observable } from "rxjs";
+import { ConfigMap, ConfigMapDetail, ConfigMapList } from "./resource.types";
 
 @Injectable()
 export class ResourceService {
+  constructor(private http: HttpClient) {
 
+  }
+
+  createConfigMap(configMap: ConfigMap): Observable<any> {
+    return this.http.post(`/api/v1/configmaps`, configMap.postBody(), {observe: "response"})
+  }
+
+  getConfigMapDetail(configMapName, projectName: string): Observable<ConfigMapDetail> {
+    return this.http.get(`/api/v1/configmaps/${configMapName}`, {
+      observe: "response", params: {
+        project_name: projectName
+      }
+    }).map((res: HttpResponse<Object>) => ConfigMapDetail.createFromRes(res.body))
+  }
+
+  deleteConfigMap(configMapName, projectName: string): Observable<any> {
+    return this.http.delete(`/api/v1/configmaps/${configMapName}`, {
+      observe: "response", params: {
+        project_name: projectName
+      }
+    })
+  }
+
+  getConfigMapList(projectName: string, pageIndex, pageSize: number): Observable<Array<ConfigMapList>> {
+    return this.http.get<Array<Object>>(`/api/v1/configmaps`, {
+      observe: "response", params: {
+        project_name: projectName,
+        configmap_list_page: pageIndex.toString(),
+        configmap_list_page_size: pageSize.toString()
+      }
+    }).map((res: HttpResponse<Array<Object>>) => {
+      let result = Array<ConfigMapList>();
+      res.body.forEach((configMap: Object) => result.push(ConfigMapList.createFromRes(configMap)));
+      console.log(result);
+      return result;
+    })
+  }
 }
