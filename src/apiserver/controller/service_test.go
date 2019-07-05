99a3a5ee28274d858bc483f00f5bc7ec24d8e6d4
luotian <luotian@inspur.com>
2019-07-05 16:32:12 +0800

    auto unit test for statefulset service

--- src/apiserver/controller/service_test.go
+++ src/apiserver/controller/service_test.go
@@ -1,6 +1,8 @@
 package controller_test
 
 import (
+	"git/inspursoft/board/src/common/model"
+	"git/inspursoft/board/src/common/utils"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -8,6 +10,33 @@ import (
 	"github.com/astaxie/beego"
 	"github.com/stretchr/testify/assert"
 )
+
+var configServiceStep = model.ConfigServiceStep{
+	ProjectID:   1,
+	Instance:    1,
+	ServiceName: "teststatefulset001",
+	ServiceType: model.ServiceTypeStatefulSet,
+	ClusterIP:   "None",
+	ContainerList: []model.Container{
+		model.Container{
+			Name: "nginx",
+			Image: model.ImageIndex{
+				ImageName:   "library/nginx",
+				ImageTag:    "1.11.5",
+				ProjectName: "library",
+			},
+		},
+	},
+	ExternalServiceList: []model.ExternalService{
+		model.ExternalService{
+			ContainerName: "nginx",
+			NodeConfig: model.NodeType{
+				TargetPort: 80,
+				Port:       80,
+			},
+		},
+	},
+}
 
 func TestGetServiceListAction(t *testing.T) {
 	assert := assert.New(t)
@@ -17,6 +46,28 @@ func TestGetServiceListAction(t *testing.T) {
 		err := signOut("admin")
 		assert.Nil(err, "Error occurred while sign out Board")
 	}()
+
+	//get service list
+	r, _ := http.NewRequest("GET", "/api/v1/services", nil)
+	r.Header.Add("token", token)
+	w := httptest.NewRecorder()
+	beego.BeeApp.Handlers.ServeHTTP(w, r)
+	assert.Equal(w.Code, http.StatusOK, "Error occurred while testing GetServiceListAction.")
+}
+
+func TestDeployStatefulSetAction(t *testing.T) {
+	assert := assert.New(t)
+	token := signIn("admin", "123456a?")
+	assert.NotEmpty(token, "Error occurred while sign in Board")
+	defer func() {
+		err := signOut("admin")
+		assert.Nil(err, "Error occurred while sign out Board")
+	}()
+
+	t.Log("Test KubeMaster")
+	masterIP = utils.GetConfig("KUBE_MASTER_IP")
+	registryIP = utils.GetConfig("REGISTRY_IP")
+	t.Log("KUBE_MASTER_IP %s  REGISTRY_IP %s", masterIP, registryIP)
 
 	//get service list
 	r, _ := http.NewRequest("GET", "/api/v1/services", nil)
--- src/apiserver/service/servicedeploy_test.go
+++ src/apiserver/service/servicedeploy_test.go
@@ -1,6 +1,7 @@
 package service_test
 
 import (
+	"git/inspursoft/board/src/apiserver/service"
 	"git/inspursoft/board/src/common/model"
 	"testing"
 
@@ -33,15 +34,48 @@ var configServiceStep = model.ConfigServiceStep{
 	},
 }
 
+var configStatefulSet = model.ConfigServiceStep{
+	ProjectID:   1,
+	Instance:    1,
+	ServiceName: "teststatefulset001",
+	ServiceType: model.ServiceTypeStatefulSet,
+	ClusterIP:   "None",
+	ContainerList: []model.Container{
+		model.Container{
+			Name: "nginx",
+			Image: model.ImageIndex{
+				ImageName:   "library/nginx",
+				ImageTag:    "1.11.5",
+				ProjectName: "library",
+			},
+		},
+	},
+	ExternalServiceList: []model.ExternalService{
+		model.ExternalService{
+			ContainerName: "nginx",
+			NodeConfig: model.NodeType{
+				TargetPort: 80,
+				Port:       80,
+			},
+		},
+	},
+}
+
 // TODO: unit test case later
 // TestDeployStatefulSet
 func TestDeployStatefulSet(t *testing.T) {
-	//assert := assert.New(t)
-	//id, err := service.DeployStatefulSet(&configServiceStep, )
+	assert := assert.New(t)
+	//deployStatefulSetInfo, err := service.DeployStatefulSet(&configServiceStep, registryBaseURI())
 	//assert.Nil(err, "Failed, err when create test image.")
 	//assert.NotEqual(0, id, "Failed to assign a image id")
 	//testImageid = id
-	t.Log(configServiceStep)
+	//t.Log(deployStatefulSetInfo)
+	t.Log("Test KubeMaster")
+	masterIP = utils.GetConfig("KUBE_MASTER_IP")
+	registryIP = utils.GetConfig("REGISTRY_IP")
+	t.Log("KUBE_MASTER_IP %s  REGISTRY_IP %s", masterIP, registryIP)
+
+	t.Log("Tested TestDeployStatefulSet")
 }
 
 // TODO: clean test
