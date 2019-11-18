b3196304c22f7766ad54c34c2b943fbb95570cd4
Liyanqing <liyanq528@163.com>
2019-11-18 15:19:18 +0800

    change copy path of kubconfig

--- tests/hosts.gitlab.sh
+++ tests/hosts.gitlab.sh
@@ -11,7 +11,8 @@ cat make/config/apiserver/kubeconfig
 echo "$KUBE_MASTER_IP"
 echo "$KUBE_MASTER_PORT"
 
-echo "cp make/config/apiserver/kubeconfig tests/"
-cp make/config/apiserver/kubeconfig tests/
+echo "cp make/config/apiserver/kubeconfig ."
+cp make/config/apiserver/kubeconfig .
+cat kubeconfig
 sed -i 's/server: .*$/server: http:\/\/'"$KUBE_MASTER_IP"':'"$KUBE_MASTER_PORT"'/' kubeconfig
 cat kubeconfig
