2f95d137510a0bed49f357ae005cd50e930bc3ed
root <root@board.maoze.org>
2020-02-06 18:40:17 +0800

    Add image exist api v2

--- src/apiserver/v2/controllers/images/supplements.go
+++ src/apiserver/v2/controllers/images/supplements.go
@@ -1,6 +1,15 @@
 package images
 
 import (
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+	"path/filepath"
+	"strings"
+
+	"git/inspursoft/board/src/apiserver/service"
+	"git/inspursoft/board/src/apiserver/v2/models/images/vm"
+
 	"github.com/astaxie/beego"
 )
 
@@ -37,13 +46,63 @@ func (c *SupplementController) CleanConfig() {
 
 // @Title Check existing about building image
 // @Description Check existing about building image
-// @Param	project_id	path	int	true	"ID of project"
-// @Param 	body	body	models.images.vm.Image	true	"Request models for image."
-// @Success 200 Successful executed.
+// @Param	project_id	path	string	true	"Name of project"
+// @Param 	body	body	vm.Image	true	"Request models for image."
+// @Success 200 Image not existing.
 // @Failure 400 Bad requests.
 // @Failure 401 Unauthorized.
 // @Failure 403 Forbidden.
-// @router /:project_id/exists [post]
+// @Failure 409 Image existing.
+// @router /:project_name/exists [post]
 func (c *SupplementController) Exists() {
+	//Get project name
+	projectName := strings.TrimSpace(c.Ctx.Input.Param(":project_name"))
 
+	//TODO
+	//Check user privilege
+	//service.IsProjectMemberByName(projectName, c.CurrentUser.ID)
+
+	//Get image name and tag
+	data, err := ioutil.ReadAll(c.Ctx.Request.Body)
+	if err != nil {
+		c.Ctx.ResponseWriter.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	body := vm.Image{}
+	err = json.Unmarshal(data, &body)
+	if err != nil {
+		c.Ctx.ResponseWriter.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	if body.Name == "" || body.Tag == "" {
+		c.Ctx.ResponseWriter.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	//Check registry
+	currentName := filepath.Join(projectName, body.Name)
+	repoList, err := service.GetRegistryCatalog()
+	if err != nil {
+		c.Ctx.ResponseWriter.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	for _, imageRegistry := range repoList.Names {
+		if imageRegistry == currentName {
+			//check tag
+			tagList, err := service.GetRegistryImageTags(currentName)
+			if err != nil {
+				c.Ctx.ResponseWriter.WriteHeader(http.StatusInternalServerError)
+				return
+			}
+			for _, tagID := range tagList.Tags {
+				if body.Tag == tagID {
+					c.Ctx.ResponseWriter.WriteHeader(http.StatusConflict)
+					return
+				}
+			}
+		}
+	}
+	c.Ctx.ResponseWriter.WriteHeader(http.StatusOK)
+	return
 }
