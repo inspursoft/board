0c9dc8b40903b45579937e9561437ddfe4d046c3
luotian <luotian@inspur.com>
2020-02-11 15:49:17 +0800

    nodegroup add for api v2 resolve body

--- src/apiserver/controllers/nodegroups/commons.go
+++ src/apiserver/controllers/nodegroups/commons.go
@@ -1,6 +1,8 @@
 package nodegroups
 
 import (
+	"encoding/json"
+
 	"fmt"
 	"git/inspursoft/board/src/apiserver/models"
 	"git/inspursoft/board/src/apiserver/service"
@@ -11,6 +13,8 @@ import (
 	"github.com/astaxie/beego"
 
 	"github.com/astaxie/beego/logs"
+	"io/ioutil"
+
 )
 
 // Operations about node groups
@@ -45,10 +49,24 @@ func (c *CommonController) List() {
 func (c *CommonController) Add() {
 	var reqNodeGroup models.nodegroups.NodeGroup
 	var err error
-	err = n.resolveBody(&reqNodeGroup)
+//	err = c.resolveBody(&reqNodeGroup)
+//	if err != nil {
+//		return
+//	}
+
+//  TODO Use base.controller later
+	data, err := ioutil.ReadAll(c.Ctx.Request.Body)
 	if err != nil {
+		c.Ctx.ResponseWriter.WriteHeader(http.StatusInternalServerError)
 		return
 	}
+
+	err = json.Unmarshal(data, &reqNodeGroup)
+	if err != nil {
+		c.Ctx.ResponseWriter.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
 
 	if !utils.ValidateWithLengthRange(reqNodeGroup.GroupName, 1, 63) {
 		n.customAbort(http.StatusBadRequest, "NodeGroup Name must be not empty and no more than 63 characters ")
