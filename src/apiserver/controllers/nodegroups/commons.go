83c81350cffe09246213b0018e8be9daf1bfec4f
luotian <luotian@inspur.com>
2020-02-21 16:39:49 +0800

    fix models nodegroups path

--- src/apiserver/controllers/nodegroups/commons.go
+++ src/apiserver/controllers/nodegroups/commons.go
@@ -4,7 +4,7 @@ import (
 	"encoding/json"
 
 	"fmt"
-	"git/inspursoft/board/src/apiserver/models"
+	"git/inspursoft/board/src/apiserver/models/nodegroups"
 	"git/inspursoft/board/src/apiserver/service"
 	"git/inspursoft/board/src/common/utils"
 	"net/http"
@@ -12,9 +12,9 @@ import (
 
 	"github.com/astaxie/beego"
 
-	"github.com/astaxie/beego/logs"
 	"io/ioutil"
 
+	"github.com/astaxie/beego/logs"
 )
 
 // Operations about node groups
@@ -47,25 +47,25 @@ func (c *CommonController) List() {
 // @Failure 500 Internal errors.
 // @router / [post]
 func (c *CommonController) Add() {
-	var reqNodeGroup models.nodegroups.NodeGroup
+	var reqNodeGroup nodegroups.NodeGroup
 	var err error
 	err = c.ResolveBody(&reqNodeGroup)
 	if err != nil {
 		return
 	}
 
-// //  TODO Use base.controller later
-// 	data, err := ioutil.ReadAll(c.Ctx.Request.Body)
-// 	if err != nil {
-// 		c.Ctx.ResponseWriter.WriteHeader(http.StatusInternalServerError)
-// 		return
-// 	}
+	// //  TODO Use base.controller later
+	// 	data, err := ioutil.ReadAll(c.Ctx.Request.Body)
+	// 	if err != nil {
+	// 		c.Ctx.ResponseWriter.WriteHeader(http.StatusInternalServerError)
+	// 		return
+	// 	}
 
-// 	err = json.Unmarshal(data, &reqNodeGroup)
-// 	if err != nil {
-// 		c.Ctx.ResponseWriter.WriteHeader(http.StatusBadRequest)
-// 		return
-// 	}
+	// 	err = json.Unmarshal(data, &reqNodeGroup)
+	// 	if err != nil {
+	// 		c.Ctx.ResponseWriter.WriteHeader(http.StatusBadRequest)
+	// 		return
+	// 	}
 
 	if !utils.ValidateWithLengthRange(reqNodeGroup.GroupName, 1, 63) {
 		n.customAbort(http.StatusBadRequest, "NodeGroup Name must be not empty and no more than 63 characters ")
