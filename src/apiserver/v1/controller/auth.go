ac581cc522d3c71df5f0418629745ef41954c7a7
zhanghu01 <zhanghu01@inspur.com>
2020-06-22 17:35:25 +0800

    format the auth.go file

--- src/apiserver/v1/controller/auth.go
+++ src/apiserver/v1/controller/auth.go
@@ -6,9 +6,10 @@ import (
 	"git/inspursoft/board/src/apiserver/service"
 	"git/inspursoft/board/src/common/model"
 	"git/inspursoft/board/src/common/utils"
-	"github.com/astaxie/beego/logs"
 	"net/http"
 	"strings"
+
+	"github.com/astaxie/beego/logs"
 )
 
 type AuthController struct {
