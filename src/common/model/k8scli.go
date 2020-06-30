c6b6127d07d9419215595326281579d94f06d8d4
wei_ting <wei_ting@inspur.com>
2020-06-30 18:18:33 +0800

    update obtain service status function.

--- src/common/model/k8scli.go
+++ src/common/model/k8scli.go
@@ -25,11 +25,28 @@ const (
 type PodPhase string
 
 const (
-	PodPending   PodPhase = "Pending"
-	PodRunning   PodPhase = "Running"
+	//The Pod has been accepted by the Kubernetes system, but one or more of the
+	//Container images has not been created. This includes time before being
+	//scheduled as well as time spent downloading images over the network, which
+	//could take a while.
+	PodPending PodPhase = "Pending"
+
+	//The Pod has been bound to a node, and all of the Containers have been
+	//created. At least one Container is still running, or is in the process of
+	//starting or restarting.
+	PodRunning PodPhase = "Running"
+
+	//All Containers in the Pod have terminated in success, and will not be restarted.
 	PodSucceeded PodPhase = "Succeeded"
-	PodFailed    PodPhase = "Failed"
-	PodUnknown   PodPhase = "Unknown"
+
+	//All Containers in the Pod have terminated, and at least one Container has
+	//terminated in failure. That is, the Container either exited with non-zero status
+	//or was terminated by the system.
+	PodFailed PodPhase = "Failed"
+
+	//For some reason the state of the Pod could not be obtained, typically due to an
+	//error in communicating with the host of the Pod.
+	PodUnknown PodPhase = "Unknown"
 )
 
 type ResourceName string
