cdb38ae65f48263e443d07868bd1f6795f67a102
tongjian li <litongjian@inspur.com>
2020-09-27 12:43:24 +0800

    adminserver monitor and ssh port issues fixing

--- src/adminserver/service/monitor.go
+++ src/adminserver/service/monitor.go
@@ -8,7 +8,6 @@ import (
 
 //GetMonitor returns Board containers' information.
 func GetMonitor() ([]*models.Boardinfo, error) {
-
 	command := `docker ps -a --format "table {{.ID}}\t{{.Image}}\t{{.CreatedAt}}\t{{.Status}}\t{{.Ports}}" | grep -v adminserver | grep ` + models.ImagePrefix
 	resp, err := Execute(command)
 	if err != nil {
@@ -21,7 +20,15 @@ func GetMonitor() ([]*models.Boardinfo, error) {
 	containersAdd := make([]*models.Boardinfo, row)
 	containersVal := make([]models.Boardinfo, row)
 
-	command2 := `docker stats -a --no-stream --format "table {{.Name}}\t{{.CPUPerc}}\t{{.MemUsage}}\t{{.NetIO}}\t{{.BlockIO}}\t{{.MemPerc}}\t{{.PIDs}}" | grep -v adminserver | grep ` + models.ContainerPrefix
+	containerPre := models.ContainerPrefix
+	_, devopsOpt, err := GetFileFromDevopsOpt()
+	if err != nil {
+		return nil, err
+	}
+	if devopsOpt == "legacy" {
+		containerPre = "archive"
+	}
+	command2 := `docker stats -a --no-stream --format "table {{.Name}}\t{{.CPUPerc}}\t{{.MemUsage}}\t{{.NetIO}}\t{{.BlockIO}}\t{{.MemPerc}}\t{{.PIDs}}" | grep -v adminserver | grep ` + containerPre
 	resp2, err := Execute(command2)
 	if err != nil {
 		return nil, err
